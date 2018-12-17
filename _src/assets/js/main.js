0d58018bdb644a184f48743c72effb5cfe442e86
JapocoinYenHoang <japocoin@gmail.com>
2018-12-17 22:19:24 +0100

    succesful null image

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -4,23 +4,24 @@ const toSearch = document.getElementById('search__text');
 const searchButton = document.querySelector('.search__btn');
 const destination = document.querySelector('.result__list');
 
-// const searching =event =>{
-//   const seachField = event.currentTarget;
-//   paco = searchField.value;
-// }
-
-
 function search() {
   fetch(`http://api.tvmaze.com/search/shows?q=${toSearch.value}`)
 
     .then(res => res.json())
     .then(data => {
-      // console.log(data);
-      // console.log(data.show);
+      destination.innerHTML='';
 
       for (let c of data) {
+        let finalImage;
+        if (c.show.image){
+          if (c.show.image.medium){
+            finalImage=c.show.image.medium;
+          }
+        }else{
+          finalImage='https://via.placeholder.com/210x295/cccccc/666666/?text=TV';
+        }
         destination.innerHTML+=`<div class="result__element--container"><li class="result__element">
-      <img class="result__element--image" src="${c.show.image.medium}" alt="">
+      <img class="result__element--image" src="${finalImage}" alt="">
       <p class="result__element--title">${c.show.name}</p>
       </li></div>`;
         // console.log(c.show);
